270c1bf0fbecc5c1a8429b46ccf670b067f949a7
Hendrik Otto <he27otto@gmail.com>
2013-06-08 18:20:32 +0200

    Last Homework done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 ControllerExercise::Application.routes.draw do
 
+resources :products
 
-  get '/products' => 'products#index'
-  get '/products/new' => 'products#new'
-  post '/products' => 'products#create'
 
   resources :users
 
